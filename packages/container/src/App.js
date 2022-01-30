4794b0ba8003fb2ecab82fdb1cf799233eb3215e
shubham1811 <shubham.h.kumar123@gmail.com>
2022-01-30 20:47:34 +0530

    inavation Step

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
-    console.log("Shubham.kumatr1");
+    console.log("Shubham.kumatr");
     return (
         <div>
             <h1>
