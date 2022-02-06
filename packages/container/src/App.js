16d1c8ba9369b05de07bfadb6c7c59a2fa4f14d9
shubham1811 <shubham.h.kumar123@gmail.com>
2022-02-06 19:11:10 +0530

    MUI Import Changes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
-import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
