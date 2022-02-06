89cb713da93c61f3385b98004c677dcb53414ed8
shubham1811 <shubham.h.kumar123@gmail.com>
2022-02-06 15:43:19 +0530

    CSS prefix changes in Marketing and container APP

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,17 @@
-import { StylesProvider } from '@material-ui/core';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 import React from 'react';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma'
+});
+
 export default () => {
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                     <Switch>
                         <Route path="/pricing" component={Pricing} />
